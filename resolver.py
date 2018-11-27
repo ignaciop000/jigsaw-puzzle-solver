6eb749097c767707841ce033439819f3816b4279
Nacho <ignaciop000@gmail.com>
2018-11-27 18:13:28 -0300

    utils

--- resolver.py
+++ resolver.py
@@ -1,6 +1,8 @@
 ##USAGE
 ##	python resolver.py --images images
+##	python resolver.py --images pieces
 import argparse
+import imutils
 import os
 import sys
 import cv2
@@ -12,5 +14,14 @@ args = vars(ap.parse_args())
 
 files = os.listdir(args["images"])
 for filename in files:
-	print os.path.join('images', filename)
-	img = cv2.imread(os.path.join('images', filename))
+	#Leer imagen, fondo oscuro, Hoja blanca y las piezas
+	print os.path.join(args["images"], filename)
+	image = cv2.imread(os.path.join(args["images"], filename))
+	#Obtener contorno de la hoja. 
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (5, 5), 0)
+	edged = cv2.Canny(gray, 75, 200)
+	#Normalizar hoja
+	#Divir piezas
+	
+	#Apply the four point transform to obtain a top-down
